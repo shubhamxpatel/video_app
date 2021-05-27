d5daa62af9ebc0eaa5d1fb9639837dbc4cdda2ba
shubhamxpatel <sp1651999@gmai.com>
2021-05-27 17:29:26 +0530

    dsvghcvbkjdsncb

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var app = require('./bin/www').app;
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.use(cors({ origin: ['http://3.143.222.94'] }))
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
